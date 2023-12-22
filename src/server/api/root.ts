27e8210dacfd24aa283da12eab25cb1c80e17a00
JaeholsUni <jameswh@iinet.net.au>
2023-12-22 14:30:41 +0800

    Mild code cleanup to get ready for ex architecture

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import {foodDiaryRouter} from "~/server/api/routers/post";
+import {foodEntryRouter} from "~/server/api/routers/foodEntry";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  foodDiary: foodDiaryRouter,
+  foodDiary: foodEntryRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/foodEntry.ts
+++ src/server/api/routers/foodEntry.ts
@@ -1,20 +1,29 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { env } from "~/env";
 
-let post = {
-    id: 1,
-    name: "Hello World",
+const apiVersion = "v1/";
+const baseUrl: string = env.BASE_API_URL + apiVersion + "food-diary/";
+
+type FoodDiaryEntry = {
+    entryId: string;
+    userId: string;
+    entryTime: string;
+    mealDescription: string;
+    additionalComments: string;
+    kilojoules: number;
 };
 
-export const foodEntry = createTRPCRouter({
-    hello: publicProcedure
-        .input(z.object({ text: z.string() }))
-        .query(({ input }) => {
-            return {
-                greeting: `Hello ${input.text}`,
-            };
-        }),
-
-
+export const foodEntryRouter = createTRPCRouter({
+  getFoodDiaryEntry: publicProcedure
+    .input(z.object({ id: z.string().uuid() }))
+      .query(async ({ input }): Promise<FoodDiaryEntry> => {
+        const response = await fetch(baseUrl+`entry/${input.id}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch food diary entry');
+        }
+        const diaryEntry: FoodDiaryEntry = await response.json() as FoodDiaryEntry;
+        return diaryEntry;
+   }),
 });
