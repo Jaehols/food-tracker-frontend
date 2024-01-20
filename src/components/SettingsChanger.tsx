d20bdcf2a4805ba56619da89e85026fcf41becf5
JaeholsUni <jameswh@iinet.net.au>
2024-01-20 15:48:27 +1100

    Need to pull another branch

--- src/components/SettingsChanger.tsx
+++ src/components/SettingsChanger.tsx
@@ -30,30 +30,13 @@ const SettingsChanger: React.FC<SettingsChangerProps> = ({ anchorEl}) => {
     const style: React.CSSProperties = {
         position: 'absolute',
         top: (anchorEl?.offsetTop ?? 0) + (anchorEl?.offsetHeight ?? 0),
-        left: (anchorEl?.offsetLeft ?? 0) - 50, // Adjust this value to move to the left
+        left: (anchorEl?.offsetLeft ?? 0) - 50,
     };
 
-
-    const tailwindColors = {
-        primary: `${theme}-primary`,
-        secondary: `${theme}-secondary`,
-    };
-
-    const CustomSwitch = styled(Switch)(({ theme }) => ({
-        '& .MuiSwitch-switchBase.Mui-checked': {
-            color: tailwindColors.primary,
-            '&:hover': {
-                backgroundColor: tailwindColors.secondary,
-            },
-        },
-        '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
-            backgroundColor: tailwindColors.primary,
-        },
-    }));
 
     return (
         <div style={style} className={`bg-${theme}-primary shadow-lg rounded-lg p-4`}>
-            <label className={`text-${theme}-secondary`}>
+            <label className={`text-${theme}-accentOne p-3`}>
                 Toggle Energy
                 <Switch
                     classes={{
@@ -68,10 +51,16 @@ const SettingsChanger: React.FC<SettingsChangerProps> = ({ anchorEl}) => {
                     checked={isEnergyOn}
                     onChange={handleEnergyToggle}
                 />
+                <input
+                    className={`mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-${theme}-secondary before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-${theme}-secondary after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-${theme}-secondary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]`}
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                />
             </label>
 
             {isEnergyOn && (
-                <div className={`text-${theme}-accentOne`}>
+                <div className={`text-${theme}-accentOne p-3`}>
                     <label>
                         <ToggleButtonGroup
                             value={energyUnit}
